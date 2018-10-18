e0e2d9e9a6bc113c42b9e37a2f43e80cfa8e0543
LifeH <hwie.zhu@gmail.com>
2018-10-17 22:03:01 -0400

    first commit

---
+++ src/Login/index.js
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+// import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import "../Login/style.css";
+
+export default class Test extends Component {
+  constructor() {
+    super()
+    this.state = {
+      posts: []
+    }
+  }
+
+  componentDidMount = async () => {
+    this.fetchPosts();
+  }
+
+  fetchPosts = async () => {
+    const response = await fetch(`https://animo-news.herokuapp.com/api/posts`);
+    const responseBody = await response.json();
+    const id = responseBody.posts[0].id;
+    this.setState({
+      posts: responseBody.posts,
+    })
+    // console.log(responseBody.posts[1].post);
+    // console.log(responseBody.posts[1].created_at.split("T")[0]);
+  }
+
+  renderPost = () => {
+    return this.state.posts.map(post => {
+      return (
+        <div key={post.id}>
+          <div className="title">{post.post}</div>
+          <div className="day-of-post">{post.created_at.split("T")[0]}</div>
+        </div>
+      )
+    })
+  }
+
+  render() {
+    return (
+      <div className="abc">
+        {this.renderPost()}
+      </div>
+    );
+  }
+}
+
+
